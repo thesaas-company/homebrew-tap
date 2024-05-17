e4cf2dee02578543be17e47f05b93d6e2f9a5c4e
TQ <founder@textquery.dev>
2024-05-17 15:40:32 +0530

    Brew formula update for sherlock version v0.0.27

--- Formula/sherlock.rb
+++ Formula/sherlock.rb
@@ -5,20 +5,20 @@
 class Sherlock < Formula
   desc "sherlock is a command line tool to interact with a datasherlocks cloud."
   homepage "https://datasherlocks.io/docs"
-  version "0.0.25"
+  version "0.0.27"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/datasherlocks/sherlock/releases/download/v0.0.25/sherlock_Darwin_arm64.tar.gz"
-      sha256 "102a45ea74b06f082729d0a58aa2e464f6840c751c0929a5df5643cac735141d"
+    on_intel do
+      url "https://github.com/thesaas-company/sherlock/releases/download/v0.0.27/sherlock_Darwin_x86_64.tar.gz"
+      sha256 "bd789f7fb3ce1370abeaea865d14bddd5b1a1255a45b2fc2b6f3439561a51dda"
 
       def install
         bin.install "sherlock"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/datasherlocks/sherlock/releases/download/v0.0.25/sherlock_Darwin_x86_64.tar.gz"
-      sha256 "60ee55b4ac3d6bce39ac940ac544685401010c7267ce2bc938b435a37a4c568d"
+    on_arm do
+      url "https://github.com/thesaas-company/sherlock/releases/download/v0.0.27/sherlock_Darwin_arm64.tar.gz"
+      sha256 "37173394502ff127b1b1edb48ec8abc3c2a1c6faee4b7ae0834338b641d937ee"
 
       def install
         bin.install "sherlock"
@@ -27,20 +27,24 @@ class Sherlock < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/datasherlocks/sherlock/releases/download/v0.0.25/sherlock_Linux_arm64.tar.gz"
-      sha256 "58c3a4d09a17830d4340caa7c827c0b30e5606488c0ba0fccb040c699eaa7cfc"
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/thesaas-company/sherlock/releases/download/v0.0.27/sherlock_Linux_x86_64.tar.gz"
+        sha256 "328e2e3fe67b155043326a77a2b1bc63aca0e04151720f5b2fec091a7f195031"
 
-      def install
-        bin.install "sherlock"
+        def install
+          bin.install "sherlock"
+        end
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/datasherlocks/sherlock/releases/download/v0.0.25/sherlock_Linux_x86_64.tar.gz"
-      sha256 "19dcb5719da9fe99c1f38bbaeff3317e9e25ef5857c7d337e271fd1bf4828a25"
+    on_arm do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/thesaas-company/sherlock/releases/download/v0.0.27/sherlock_Linux_arm64.tar.gz"
+        sha256 "dbfbd2520619e9af12d1389228d3bfb6ca2db780c9f43459b42a3718971aee85"
 
-      def install
-        bin.install "sherlock"
+        def install
+          bin.install "sherlock"
+        end
       end
     end
   end
